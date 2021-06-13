ee77614c30acbba141dab20e4a56480349d91613
LavaSquids <79622877+LavaSquids@users.noreply.github.com>
2021-06-13 11:16:10 +0100

    Improve startup & fix for staff-tags

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Plugin } from '@vizality/entities';
 import { Events } from '@vizality/constants';
 import { patch, unpatchAll } from '@vizality/patcher';
 import { getModule } from '@vizality/webpack';
+import { sleep } from '@vizality/util/time';
 
 import getNick from './api/getNick';
 
@@ -9,8 +10,12 @@ const { getGuildId } = getModule('getGuildId', 'getLastSelectedGuildId');
 
 export default class extends Plugin {
   start () {
-    vizality.manager.plugins.once(Events.VIZALITY_ADDONS_READY, () => {
-      this.patch();
+    vizality.manager.plugins.once(Events.VIZALITY_ADDONS_READY, this.patch);
+    sleep(1000).then(() => {
+      if (vizality.manager.plugins.listeners(Events.VIZALITY_ADDONS_READY).includes(this.patch)) {
+        vizality.manager.plugins.off(Events.VIZALITY_ADDONS_READY, this.patch);
+        this.patch();
+      }
     });
   }
 
@@ -20,7 +25,7 @@ export default class extends Plugin {
       const userId = args[0].message.author.id;
 
       const Nick = getNick(getGuildId(), userId, 21);
-      if (Nick) args[0].author.nick += ` (${userId})`;
+      if (Nick && !args[0].author.nick.includes(` (${userId})`)) args[0].author.nick += ` (${userId})`;
     }, 'before');
     patch(getModule(m => m?.displayName === 'Clickable').prototype, 'renderInner', (args, res) => {
       if (res.type === 'span' && typeof res.props.children === 'string') {
@@ -41,9 +46,9 @@ export default class extends Plugin {
       res.props.children = (e) => {
         const children = _children(e);
         if (children.props.children.props?.children[1]) {
-          children.props.children.props.children[1] = getNick(guildId, userId, 21, false) ?? children.props.children.props.children[1];
+          children.props.children.props.children[1] = getNick(guildId, userId, 21, undefined, false) ?? children.props.children.props.children[1];
         } else if (children.props.children) {
-          children.props.children = getNick(guildId, userId, 21, true) ?? children.props.children;
+          children.props.children = getNick(guildId, userId, 21, undefined, true) ?? children.props.children;
         }
         return children;
       };
@@ -59,9 +64,15 @@ export default class extends Plugin {
     // Member List
     patch(getModule(m => m?.displayName === 'MemberListItem').prototype, 'render', (args, res) => {
       const name = res.props.name?.props.children;
+      const nameST = res.props.name?.props.children.props?.text;
       if (!name) return res;
 
-      res.props.name.props.children = getNick(getGuildId(), res.props['vz-user-id'], 20) ?? res.props.name.props.children;
+      if (nameST) {
+        res.props.name.props.children.props.text = getNick(getGuildId(), res.props['vz-user-id'], 21, 'inline') ?? nameST;
+        res.props.name.props.children.props.tooltipText = getNick(getGuildId(), res.props['vz-user-id'], 18) ?? nameST;
+      } else if (name) res.props.name.props.children = getNick(getGuildId(), res.props['vz-user-id'], 21) ?? name;
+
+      return res;
     });
   }
 
